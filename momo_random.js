46d6bc861f8ddd038f83dcdbb3492600e08aab36
小尭 <82511711+XiaoPigYao@users.noreply.github.com>
2022-09-23 11:16:41 +0800

    Update momo_random.js

--- momo_random.js
+++ momo_random.js
@@ -4,7 +4,7 @@
  const $ = new Env("墨墨背单词share_random");
  const notify = $.isNode() ? require('./sendNotify') : '';
  const Notify = 1; //0为关闭通知，1为打开通知,默认为1
- const debug = 0; //0为关闭调试，1为打开调试,默认为0
+ const debug = 1; //0为关闭调试，1为打开调试,默认为0
 
  let msg = '';
  let UA = '';
