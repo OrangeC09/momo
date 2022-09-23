5b092001d9eae0904024312dd4dd98725d24426b
小尭 <82511711+XiaoPigYao@users.noreply.github.com>
2022-09-23 09:07:03 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -135,7 +135,7 @@ if (process.env.PUSH_PLUS_USER) {
 
 async function sendNotify(text, desp, params = {}) {
   //提供6种通知
-  desp += `\n感谢xzy`;
+  //desp += `\n感谢文静`;
   await Promise.all([
     serverNotify(text, desp),//微信server酱
     pushPlusNotify(text, desp)//pushplus(推送加)
